98cb7125148a00cb1e0d22187af3a9a642652923
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-30 11:07:59 +0530

    Requirement 1: Add. Added the required testcases and logic to checkFizzBuzz according to Requirement 1.

--- src/main/java/com/m2p/CheckFizzBuzz.java
+++ src/main/java/com/m2p/CheckFizzBuzz.java
@@ -1,4 +1,29 @@
 package com.m2p;
 
 public class CheckFizzBuzz {
+
+
+    public String getFizzBuzz(int number) {
+
+        if(number <= 0 || number > 100)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        if(number % 3 == 0 && number % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+
+        if(number % 3 == 0)
+        {
+            return "Fizz";
+        }
+
+        if(number % 5 == 0)
+        {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
 }
--- src/test/java/com/m2p/CheckFizzBuzzTest.java
+++ src/test/java/com/m2p/CheckFizzBuzzTest.java
@@ -1,7 +1,67 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CheckFizzBuzzTest {
 
     CheckFizzBuzz fizzBuzzObject = new CheckFizzBuzz();
+
+    @Test
+    void toReturnFizzWhenDivisibleByThree()
+    {
+        //Arrange
+        String expectedValue = "Fizz";
+
+        //Act
+        String actualValue = fizzBuzzObject.getFizzBuzz(18);
+
+        //Assert
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnBuzzWhenDivisibleByFive()
+    {
+        String expectedValue = "Buzz";
+
+        String actualValue = fizzBuzzObject.getFizzBuzz(25);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnFizzBuzzWhenDivisibleByBothThreeAndFive()
+    {
+        String expectedValue = "FizzBuzz";
+
+        String actualValue = fizzBuzzObject.getFizzBuzz(30);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnTheNumberItselfWhenSixTeenIsEntered()
+    {
+        String expectedValue = "16";
+
+        String actualValue = fizzBuzzObject.getFizzBuzz(16);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnExceptionWhenMinusFiveIsEntered()
+    {
+        assertThrows(IllegalArgumentException.class, ()-> fizzBuzzObject.getFizzBuzz(-5));
+    }
+
+    @Test
+    void toReturnExceptionWhenHundredAndFiveIsEntered()
+    {
+        assertThrows(IllegalArgumentException.class, ()->fizzBuzzObject.getFizzBuzz(105));
+    }
 
 }
